c12c2a2214b5ede5d0a5291685597120fc766b9d
David Karl <david.karl@tec.govt.nz>
2018-04-23 11:47:48 +1200

    First testable release of forged items

--- scripts/generateObjects.js
+++ scripts/generateObjects.js
@@ -6,7 +6,7 @@ let nextObjectNumber = Number(fs.readFileSync('../objects/nextObjectNumber.txt')
 for (const d of data) {
     const objectName = d.substr(0, d.indexOf('\t'));
     const copiedObjectId = d.replace(`${objectName}\t`, '');
-    const copiedObjectFileContents = fs.readFileSync(`${copiedObjectId}.txt`).toString('utf8');
+    const copiedObjectFileContents = fs.readFileSync(`../objects/${copiedObjectId}.txt`).toString('utf8');
 
     const matchCopiedObjectId = copiedObjectFileContents.match(/id=(\d+)?/);
     const matchCopiedObjectName = copiedObjectFileContents.match(/\n(.+)?\ncontainable=/);
@@ -16,8 +16,16 @@ for (const d of data) {
 
     
     // this recolours the copper anvil sprite to bronze
-    objectFileContents = objectFileContents.replace('spriteID=565\npos=0.000000,-11.000000\nrot=0.000000\nhFlip=0\ncolor=1.000000,0.723468,0.521127',
-        'spriteID=565\npos=0.000000,-11.000000\nrot=0.000000\nhFlip=0\ncolor=0.684396,0.718310,0.374330');
+    // objectFileContents = objectFileContents.replace('spriteID=20065\npos=2.000000,-29.000000\nrot=0.000000\nhFlip=0\ncolor=1.000000,0.422535,0.000000',
+    //     'spriteID=20065\npos=2.000000,-29.000000\nrot=0.000000\nhFlip=0\ncolor=0.684396,0.718310,0.374330');
+
+    // this recolours all copper to bronze
+    // objectFileContents = objectFileContents.replace('color=1.000000,0.422535,0.000000',
+    //     'color=0.684396,0.718310,0.374330');
+
+    // this recolours all hot copper to hot bronze
+    // objectFileContents = objectFileContents.replace('color=1.000000,0.723468,0.521127',
+    //     'color=1.000000,0.916683,0.830986');
 
     if (matchCopiedObjectName[1] == objectName) {
         console.log(`Skipping file; ${matchCopiedObjectName[1]} needs name updated`);
